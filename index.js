0e3b40a089431ec655be5a34e6115edbd6d50693
Rebecca Amarh-Adjei <37591664+ramarhadjei@users.noreply.github.com>
2020-08-16 17:46:16 -0400

    second commit. Added express to vanilla

--- index.js
+++ index.js
@@ -3,12 +3,20 @@ const http = require('http')
 const server = http.createServer()
 
 server.on('request', (req, res) =>{
-    console.log('hello');
-    res.writeHead(200);
-    res.write('Hello');
-    res.end();
+    console.log('this is the method', req.method);
+    if (req.method === 'GET'){
+        if(req.url === '/'){
+            res.writehead(200);
+            res.write('This is the main page');
+            res.end();
+        }else if( req.url === '/puppies'){
+            res.writeHead(200);
+            res.write('puppies page');
+            res.end();
+        }
+    }
 });
-
+    
 server.listen(3000, 'localhost');
 
 console.log('listening on Port 3000');
